ad8a18a5150e1a9f366a6b066c071355596819f9
samazjhb025 <samazjhb025@student.wethinkcode.co.za>
2026-09-14 12:08:47 +0200

    Add password length validation

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const analyzeBtn = document.getElementById("analyzeBtn");
 
-analyzeBtn.addEventListener("click", ()=>{
-    document.getElementById("result").textContent = "Password analysis comming soon....";
+analyzeBtn.addEventListener("click", () => {
+    const password = document.getElementById("passwordInput").value;
+    const result = document.getElementById("result");
+
+    if (password.length >= 8) {
+        result.textContent = "✓ Password length is valid";
+    } else {
+        result.textContent = "✗ Password must be at least 8 characters long";
+    }
 });
