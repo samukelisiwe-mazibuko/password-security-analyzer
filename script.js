a0cb201912bd66eb01fc4539136c91737e5aaea9
samazjhb025 <samazjhb025@student.wethinkcode.co.za>
2026-09-14 12:29:59 +0200

    Check uppercase lowercase numbers and symbols

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ analyzeBtn.addEventListener("click", () => {
     const password = document.getElementById("passwordInput").value;
     const result = document.getElementById("result");
 
-    if (password.length >= 8) {
-        result.textContent = "✓ Password length is valid";
-    } else {
-        result.textContent = "✗ Password must be at least 8 characters long";
-    }
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecial = /[^A-Za-z0-9]/.test(password);
+
+    result.innerHTML = `
+    Length: ${password.length >= 8 ? "Pass" : "Fail"}<br>
+    Uppercase: ${hasUppercase ? "Pass" : "Fail"}<br>
+    Lowercase: ${hasLowercase ? "Pass" : "Fail"}<br>
+    Number: ${hasNumber ? "Pass" : "Fail"}<br>
+    Special Character: ${hasSpecial ? "Pass" : "Fail"}
+`;
 });
