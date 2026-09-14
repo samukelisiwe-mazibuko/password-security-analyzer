17429a9f29a844d8f6e47cb109b63bfc435e7548
samazjhb025 <samazjhb025@student.wethinkcode.co.za>
2026-09-14 12:59:34 +0200

    Add password strength scoring

--- script.js
+++ script.js
@@ -9,11 +9,33 @@ analyzeBtn.addEventListener("click", () => {
     const hasNumber = /[0-9]/.test(password);
     const hasSpecial = /[^A-Za-z0-9]/.test(password);
 
+    let score = 0;
+
+    if (password.length >= 8) {
+        score ++;
+    }
+
+    if (hasUppercase){
+        score ++;
+    }
+
+    if(hasLowercase){
+        score ++;
+    }
+
+    if (hasNumber) {
+        score ++;
+    }
+
+    if(hasSpecial){
+        score++;
+    }
+
     result.innerHTML = `
-    Length: ${password.length >= 8 ? "Pass" : "Fail"}<br>
-    Uppercase: ${hasUppercase ? "Pass" : "Fail"}<br>
-    Lowercase: ${hasLowercase ? "Pass" : "Fail"}<br>
-    Number: ${hasNumber ? "Pass" : "Fail"}<br>
-    Special Character: ${hasSpecial ? "Pass" : "Fail"}
-`;
+        Length: ${password.length >= 8 ? "Pass" : "Fail"}<br>
+        Uppercase: ${hasUppercase ? "Pass" : "Fail"}<br>
+        Lowercase: ${hasLowercase ? "Pass" : "Fail"}<br>
+        Number: ${hasNumber ? "Pass" : "Fail"}<br>
+        Special Character: ${hasSpecial ? "Pass" : "Fail"}
+    `;
 });
